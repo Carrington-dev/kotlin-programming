3585f3608835711b76d059ba30a4387b22614a99
Carrington-dev <crn96m@gmail.com>
2024-09-25 20:05:13 +0200

    Else if

--- variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/UserInputs.kt
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/UserInputs.kt
@@ -4,7 +4,12 @@ fun main(){
     var age =  readln().toInt()
     if ( age < 18){
             println("You are too young, and you can not get an account")
-    }else{
+    } else if ( age > 60){
+        println("You are on retirement, vhagai vha duhulu vhavho?")
+    } else if (age in 50..59){
+        println("You are preparing for retirement, vhagai vha duhulu vhavho?")
+    }
+    else{
             println("Move to the next open counter")
     }
 
