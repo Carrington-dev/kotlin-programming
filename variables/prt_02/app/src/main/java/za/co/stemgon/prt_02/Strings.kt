8a64c6495c8eb3c22306205896edb1891ef39591
Carrington-dev <crn96m@gmail.com>
2024-09-25 19:40:32 +0200

    Added string and to upper case conversion vice versa

--- variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/Strings.kt
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/Strings.kt
@@ -1,2 +1,16 @@
 package za.co.stemgon.prt_02
 
+fun main(){
+    var name = "";
+    println(name)
+    name = "My name is Carrington, and I love it that way."
+    println(name)
+    println()
+    for (i in name){
+        print("$i ")
+    }
+    println()
+
+    println(name.toLowerCase())
+    println(name.toUpperCase())
+}
