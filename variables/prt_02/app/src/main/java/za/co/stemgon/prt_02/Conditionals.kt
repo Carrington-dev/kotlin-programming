a267c8c15c5c1eb693cd613e374f8539e2d292a8
Carrington-dev <crn96m@gmail.com>
2024-09-25 19:48:36 +0200

    Added Logic Conditions

---
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/Conditionals.kt
@@ -0,0 +1,17 @@
+package za.co.stemgon.prt_02
+
+fun main(){
+
+    if (2.toFloat() == 2.00000f){
+        println("They are equal")
+    }else{
+        println("They are not equal")
+    }
+
+
+    if( 2 > 2.001){
+        println("2 is greater than 2.001")
+    }else{
+        println("2 is less than 2.001")
+    }
+}
---
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/UserInputs.kt
@@ -0,0 +1,2 @@
+package za.co.stemgon.prt_02
+
