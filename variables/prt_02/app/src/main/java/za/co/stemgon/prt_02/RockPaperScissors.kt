0da20d55b7da3c1bec7c9d549637b0c68c4642e1
Carrington-dev <crn96m@gmail.com>
2024-09-25 20:19:18 +0200

    Fixed computer choice error out of index

--- variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/RockPaperScissors.kt
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/RockPaperScissors.kt
@@ -3,7 +3,11 @@ package za.co.stemgon.prt_02
 import kotlin.random.Random
 
 fun main(){
-    val computerChoice = (1..3).random()
+    val computerChoice = (0..2).random()
     val options = arrayListOf<String>("Paper", "Rock", "Scissors")
     println(options[computerChoice])
+    println(computerChoice)
+
+//    print()
+//    val myChoice =
 }
