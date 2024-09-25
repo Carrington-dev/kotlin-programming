e77cdd770541fa3f455a0fdb12e69711b6c7d3b1
Carrington-dev <crn96m@gmail.com>
2024-09-25 20:36:09 +0200

    Playing rock, paper, scissors

--- variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/RockPaperScissors.kt
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/RockPaperScissors.kt
@@ -5,12 +5,38 @@ import kotlin.random.Random
 fun main(){
     val computerChoice = (0..2).random()
     val options = arrayListOf<String>("Paper", "Rock", "Scissors")
-    println(options[computerChoice])
-    println(computerChoice)
+//    println(options[computerChoice])
+//    println(computerChoice)
 
+    println()
+    println("Choose one of the options below.")
     println("0. paper\n1. Rock\n2. Scissors")
 //    print()
-//    val myChoice = readln().toInt()
-//    println()
+    val myChoice = readln().toInt()
+//    println(options[myChoice])
 
+
+    if (options[computerChoice] == options[myChoice]){
+        println("It's a draw, play again")
+    }else if (options[myChoice] == options[0]){
+        if (options[computerChoice] == options[1]){
+            println("You won, ")
+        }else{
+            println("You lost, ")
+        }
+    }else if (options[myChoice] == options[1]){
+        if (options[computerChoice] == options[0]){
+            println("You lost, ")
+        }else{
+            println("You won, ")
+        }
+    }else{
+        if (options[computerChoice] == options[0]){
+            println("You won, ")
+        }else{
+            println("You lost, ")
+        }
+    }
+    println(options[computerChoice])
+    println(options[myChoice])
 }
