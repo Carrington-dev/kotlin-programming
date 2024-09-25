121fd51b83aa248a4e0bd27b33db93ce1897cd50
Carrington-dev <crn96m@gmail.com>
2024-09-25 19:51:56 +0200

    Reading userinput to int

--- variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/UserInputs.kt
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/UserInputs.kt
@@ -1,2 +1,11 @@
 package za.co.stemgon.prt_02
 
+fun main(){
+    var age =  readln().toInt()
+    if ( age < 18){
+            println("You are too young, and you can not get an account")
+    }else{
+            println("Move to the next open counter")
+    }
+
+}
