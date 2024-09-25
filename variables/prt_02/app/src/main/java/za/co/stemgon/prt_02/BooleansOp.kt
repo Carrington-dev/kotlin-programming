98c59e0e7636018a92e2b04c8f37254daef14415
Carrington-dev <crn96m@gmail.com>
2024-09-25 15:41:51 +0200

    Added 3 boolean examples

---
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/BooleansOp.kt
@@ -0,0 +1,10 @@
+package za.co.stemgon.prt_02
+
+fun main(){
+    val isKotlinFun: Boolean = true
+    val isFishTasty: Boolean = false
+    val isNotFishTasty: Boolean? = null
+    println(isKotlinFun)   // Outputs true
+    println(isFishTasty)   // Outputs false
+    println(isNotFishTasty)   // Outputs false
+}
