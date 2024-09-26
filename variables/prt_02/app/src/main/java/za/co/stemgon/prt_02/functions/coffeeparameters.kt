dd2a54cdb68360809837f9e502e5028427ae6b0c
Carrington-dev <crn96m@gmail.com>
2024-09-26 09:21:52 +0200

    Coffee with func params

--- variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/functions/coffeeparameters.kt
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/functions/coffeeparameters.kt
@@ -1,7 +1,7 @@
 package za.co.stemgon.prt_02.functions
 
-fun makeCoffee(name: String) {
-    println("i am preparing coffee for $name")
+fun makeCoffee(name: String, number: Int) {
+    println("i am preparing coffee for $name and I like $number spoons of sugar")
 }
 
 fun addNumber(number1: Int, number2: Int): Int {
@@ -9,6 +9,11 @@ fun addNumber(number1: Int, number2: Int): Int {
 }
 
 fun main(){
-    makeCoffee("Carr")
+    makeCoffee("Carr", 4)
+    makeCoffee("Carry", 3)
+    makeCoffee("Carrie", 6)
+    makeCoffee("Mulalo", 8)
+    makeCoffee("Him", 54)
+    makeCoffee("Her", 47)
     println(addNumber(5, 8))
 }
