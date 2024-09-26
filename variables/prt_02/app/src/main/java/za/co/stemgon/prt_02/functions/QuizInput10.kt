a687a0360f61000dd8b11d8486e5d4f41b66ec45
Carrington-dev <crn96m@gmail.com>
2024-09-26 10:27:03 +0200

    More Return Types

--- variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/functions/QuizInput10.kt
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/functions/QuizInput10.kt
@@ -4,6 +4,12 @@ fun addNumbers(number: Int, number2: Int): Int{
     return number + number2
 }
 
+// More Return Types
+
+fun divideTwoNumbers(number: Double, number2: Double): Double {
+    return  number / number2
+}
+
 fun main(){
     print("Please enter your first number: ")
     var firstNumber = readln().toInt()
@@ -12,5 +18,6 @@ fun main(){
     var secondNumber = readln().toInt()
 
     println("The sum of $firstNumber and $secondNumber is ${addNumbers(firstNumber, secondNumber)}")
+    println("The division of $firstNumber and $secondNumber is ${divideTwoNumbers(firstNumber.toDouble(), secondNumber.toDouble())}")
 
 }
