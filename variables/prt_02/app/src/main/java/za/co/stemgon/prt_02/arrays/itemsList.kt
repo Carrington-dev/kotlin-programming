c0c1fe6cd41c41d4f65b75c578f5f36f594447e5
Carrington-dev <crn96m@gmail.com>
2024-09-27 14:06:47 +0200

    Dealt with a mutable list

---
+++ variables/prt_02/app/src/main/java/za/co/stemgon/prt_02/arrays/itemsList.kt
@@ -0,0 +1,19 @@
+package za.co.stemgon.prt_02.arrays
+
+fun main(){
+    var numbers = mutableListOf<Int>()
+    println("Enter a number or q to quit")
+    var input = readln()
+
+    while (input != "q"){
+        println("Enter a number or q to quit")
+        numbers.add(input.toInt())
+        input = readln()
+    }
+
+    println()
+    println("All numbers are: ")
+    for (number in numbers){
+        println(number)
+    }
+}
