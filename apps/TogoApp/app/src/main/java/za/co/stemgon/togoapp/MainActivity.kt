a130b47eeaf0e07bedfe82bd0b923c493ad0e075
Carrington-dev <crn96m@gmail.com>
2024-10-02 21:01:59 +0200

    Added Dialogue Content v2

--- apps/TogoApp/app/src/main/java/za/co/stemgon/togoapp/MainActivity.kt
+++ apps/TogoApp/app/src/main/java/za/co/stemgon/togoapp/MainActivity.kt
@@ -11,13 +11,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,33 +48,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainAppUI(){
+    var isDialogOpen by remember {
+        mutableStateOf(false)
+    }
+    var nameOfTodoItem by remember {
+        mutableStateOf("")
+    }
+    var numberOfTodoItems by remember {
+        mutableStateOf("1")
+    }
+    var todoItems = mutableListOf<String>()
+    Button(onClick = { isDialogOpen = true }) {
+        Text(text = "Add Todo")
+
+    }
+    LazyColumn(content = {
+        items(todoItems){
+
+        }
+    })
+    Spacer(modifier = Modifier.height(8.dp))
+    if( isDialogOpen ){
+        AlertDialog(
+            onDismissRequest = {
+                    isDialogOpen = false
+            },
+            confirmButton = {
+
+                Button(onClick = { /*TODO*/
+                    isDialogOpen = false
+                }) {
+                    Text(text = "Add")
+                }
+            },
+            title = {
+                Column {
+                Text(text = "List of your todos", Modifier.padding(8.dp))
+                OutlinedTextField(value = nameOfTodoItem, onValueChange = { nameOfTodoItem = it}, modifier = Modifier.padding(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(value = numberOfTodoItems, onValueChange = { numberOfTodoItems = it}, modifier = Modifier.padding(8.dp))
+                Row(  modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ){
+
+
+
+                }
+            }
+            }
+        )
+    }
+
+}
+
+@Composable
 fun TodoListApp(){
-    Column( horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Add Todo")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
 
-        Text(text = "List of your todos", Modifier.padding(8.dp))
-        OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.padding(8.dp))
-        Spacer(modifier = Modifier.height(8.dp))
-        OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.padding(8.dp))
-        Row(  modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Cancel")
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Add Todo")
-            }
-        }
+    Column( horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        MainAppUI()
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
