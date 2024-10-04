4bb3e5799a93cf3f210c738993a3af9a65751493
Carrington-dev <crn96m@gmail.com>
2024-10-04 13:57:15 +0200

    Added APIService Interface

--- apps/RetrofitApp/app/src/main/java/za/co/stemgon/retrofitapp/services/APIService.kt
+++ apps/RetrofitApp/app/src/main/java/za/co/stemgon/retrofitapp/services/APIService.kt
@@ -1,10 +1,20 @@
 package za.co.stemgon.retrofitapp.services
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import retrofit2.http.GET
+import za.co.stemgon.retrofitapp.models.MakeResponse
+
+val retrofit = Retrofit
+    .Builder()
+    .baseUrl("https://api.vroomhive.co.za/api/v1/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+val makeAPIService = retrofit.create(APIService::class.java)
 
 interface APIService {
     @GET("makes/")
-    suspend fun getMakes(){
-
-    }
+    suspend fun getMakes(): MakeResponse
 }
---
+++ apps/RetrofitApp/app/src/main/java/za/co/stemgon/retrofitapp/viewmodel/MakeViewModel.kt
@@ -0,0 +1,6 @@
+package za.co.stemgon.retrofitapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MakeViewModel: ViewModel() {
+}
