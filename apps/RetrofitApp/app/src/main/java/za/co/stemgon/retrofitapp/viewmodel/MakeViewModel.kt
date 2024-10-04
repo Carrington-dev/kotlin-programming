1bd66099e7d90a89ccfc9cd29b01546091ee218a
Carrington-dev <crn96m@gmail.com>
2024-10-04 14:31:00 +0200

    Added MakeViewModel

--- apps/RetrofitApp/app/src/main/java/za/co/stemgon/retrofitapp/viewmodel/MakeViewModel.kt
+++ apps/RetrofitApp/app/src/main/java/za/co/stemgon/retrofitapp/viewmodel/MakeViewModel.kt
@@ -1,6 +1,45 @@
 package za.co.stemgon.retrofitapp.viewmodel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import za.co.stemgon.retrofitapp.models.Make
+import za.co.stemgon.retrofitapp.services.makeAPIService
 
 class MakeViewModel: ViewModel() {
+
+    init {
+        fetchMakes()
+    }
+
+    private  val _makeState = mutableStateOf(MakeState())
+    private val makeState: State<MakeState> = _makeState
+
+    private fun fetchMakes(){
+        viewModelScope.launch {
+            try {
+                val  response = makeAPIService.getMakes()
+                _makeState.value = _makeState.value.copy(
+                    isLoading = false,
+                    error = null,
+                    list = response.result
+                )
+
+            }catch (e: Exception){
+                _makeState.value = _makeState.value.copy(
+                    isLoading = false,
+                    error = "Error feching makes ${e.message}",
+                    list = emptyList()
+                )
+            }
+        }
+    }
+
+    data class MakeState(
+        var isLoading: Boolean = true,
+        var list: List<Make> = emptyList(),
+        var error: String? = null
+    )
 }
