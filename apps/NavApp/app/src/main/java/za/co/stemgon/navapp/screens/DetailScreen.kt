e76df7368a5fd029e19e60f689f28004a6d48785
Carrington-dev <crn96m@gmail.com>
2024-10-07 11:08:18 +0200

    Added Nav App

---
+++ apps/NavApp/app/src/main/java/za/co/stemgon/navapp/screens/DetailScreen.kt
@@ -0,0 +1,43 @@
+package za.co.stemgon.navapp.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DetailScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        Text(
+            text = "Detail Page",
+            textAlign = TextAlign.Center,
+            fontStyle = FontStyle.Normal,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.W900,
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPreview(){
+    DetailScreen()
+}
