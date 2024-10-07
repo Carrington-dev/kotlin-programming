27622a09fdc2629ab652c17cdcc5df3d579bc6db
Carrington-dev <crn96m@gmail.com>
2024-10-07 11:12:06 +0200

    Removed Example Greeting

--- apps/NavApp/app/src/main/java/za/co/stemgon/navapp/MainActivity.kt
+++ apps/NavApp/app/src/main/java/za/co/stemgon/navapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import za.co.stemgon.navapp.screens.HomeScreen
 import za.co.stemgon.navapp.ui.theme.NavAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavAppTheme {
-        Greeting("Android")
-    }
-}
